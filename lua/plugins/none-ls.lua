9626ca1af8bbd2371fc5c82777ad561b0e9690b0
Nashid Farhad <mail.farhad@gmail.com>
2024-07-05 22:47:26 +1000

    none-ls update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,38 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        require("none-ls.diagnostics.eslint_d"),
-        null_ls.builtins.formatting.stylua, -- this needs ":MasonInstall stylua"
-        -- null_ls.builtins.formatting.ruff,
-        null_ls.builtins.formatting.prettierd,
-        --null_ls.builtins.diagnostics.eslint_d,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"jay-babu/mason-null-ls.nvim",
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local mason_null_ls, null_ls, null_ls_utils =
+			require("mason-null-ls"), require("null-ls"), require("null-ls.utils")
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {}) -- shortcut for formatting
-  end,
+		mason_null_ls.setup({
+			ensure_installed = {
+				"prettierd",
+				"stylua",
+				"eslint_d",
+				"ruff",
+			},
+		})
+		local formatting = null_ls.builtins.formatting
+		-- local diagnostics = null_ls.builtins.diagnostics
+		null_ls.setup({
+			root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+			sources = {
+				-- require("none-ls.diagnostics.eslint_d"),
+				formatting.stylua, -- this needs ":MasonInstall stylua"
+				-- null_ls.builtins.formatting.ruff,
+				formatting.prettierd,
+				formatting.ruff,
+				formatting.isort,
+				--null_ls.builtins.diagnostics.eslint_d,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {}) -- shortcut for formatting
+	end,
 }
