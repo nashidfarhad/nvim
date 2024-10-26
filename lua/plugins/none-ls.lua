c0d52a4ff0c2263374c93ca59e408ee56a17b6cd
Nashid Farhad <mail.farhad@gmail.com>
2024-10-26 22:26:26 +1100

    ruff fomatting fix

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -18,6 +18,28 @@ return {
 				"ruff",
 			},
 		})
+
+		-- fix for ruff formatting
+		-- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1286k
+		local methods, helpers = require("null-ls.methods"), require("null-ls.helpers")
+		local function ruff_fix()
+			return helpers.make_builtin({
+				name = "ruff",
+				meta = {
+					url = "https://github.com/charliermarsh/ruff/",
+					description = "An extremely fast Python linter, written in Rust.",
+				},
+				method = methods.internal.FORMATTING,
+				filetypes = { "python" },
+				generator_opts = {
+					command = "ruff",
+					args = { "--fix", "-e", "-n", "--stdin-filename", "$FILENAME", "-" },
+					to_stdin = true,
+				},
+				factory = helpers.formatter_factory,
+			})
+		end
+
 		local formatting = null_ls.builtins.formatting
 		-- local diagnostics = null_ls.builtins.diagnostics
 		null_ls.setup({
@@ -27,7 +49,8 @@ return {
 				formatting.stylua, -- this needs ":MasonInstall stylua"
 				-- null_ls.builtins.formatting.ruff,
 				formatting.prettierd,
-				formatting.ruff,
+				-- formatting.ruff,
+        ruff_fix(),
 				formatting.isort,
 				--null_ls.builtins.diagnostics.eslint_d,
 			},
