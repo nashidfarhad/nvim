cf02fa87e3a0625bc4a38f703c9ee885222c8589
Nashid Farhad <nfarhad@Nashids-MacBook-Pro.local>
2026-04-08 05:38:27 +1000

    fix treesitter switch from master to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return   -- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation
   {"nvim-treesitter/nvim-treesitter", 
     build = ":TSUpdate",
     config = function ()
-      local config = require("nvim-treesitter.configs")
+      local config = require("nvim-treesitter.config")
       config.setup({
         -- ensure_installed = {"lua", "fennel", "javascript", "python", "yaml", "typescript", "tsx", "json"},
         auto_install = true,
